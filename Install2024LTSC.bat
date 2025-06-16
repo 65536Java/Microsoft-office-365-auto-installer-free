7cf9aa1fddcd90311072c66e1a43d7d632601fc3
65536 <eizoulee@gmail.com>
2025-06-16 18:43:50 +0800

    Update Install2024LTSC.bat

--- Install2024LTSC.bat
+++ Install2024LTSC.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 powershell -Command "Invoke-WebRequest -Uri 'https://download.microsoft.com/download/6c1eeb25-cf8b-41d9-8d0d-cc1dbc032140/officedeploymenttool_18730-20142.exe' -OutFile '%CD%\od.exe'"
-powershell -Command "Invoke-WebRequest -Uri 'https://drive.usercontent.google.com/download?id=1AEy4FwHK_818ohzEMLBKrrf3jeDW5You&export=download&authuser=0&confirm=t&uuid=064dda4e-ae46-4dd7-86de-bf89b8f2f26e&at=ALoNOgm27Snk0rXjHj7BjtNIUreJ:1749555888635' -OutFile '%CD%/config.xml'"
+powershell -Command "Invoke-WebRequest -Uri 'https://download1507.mediafire.com/ih4pdg9cruzg3V8Lp3ymS9_aRi7s08KWgZTWiFY9KyqZS2VqO9ikS-fLVwYLCwXS41zHs-4t8iS3mltUTcsyHNBjvGYkZoeQiwvShmkcw22GShQzQzTqlKJhTgDIBY88datsui0-92Z_OjARVDztJ8BQzZli5aG_CaCkGgiKo1x_TQ/2t03qf9nv8agxh7/config.xml' -OutFile '%CD%/config.xml'"
 od
 del configuration-Office365-x64.xml
 echo Downloading office 2024...
